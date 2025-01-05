eaa41d5cb011d2c30c98b1f83bc28ac481931a45
Pranshul Agrawal <2021kucp1101@iiitkota.ac.in>
2025-01-06 02:37:08 +0530

    refactoring codebase

--- routes/calls.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTodaysCalls } = require('../controllers/calls');
-const { protect } = require('../middleware/auth');
-
-// Get Today's Calls
-router.get('/today', protect, getTodaysCalls);
-
-module.exports = router;
--- routes/performance.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPerformanceMetrics } = require('../controllers/performance');
-const { protect } = require('../middleware/auth');
-
-// Get Performance Metrics
-router.get('/', protect, getPerformanceMetrics);
-
-module.exports = router;
